621c3e31f0619fd373026b4373099c4575b2621c
李俊达 <lijunda04@58.com>
2023-05-01 11:35:04 +0800

    修改配置

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -2,25 +2,5 @@ import { navbar } from "vuepress-theme-hope";
 
 // 精选图标：https://vuepress-theme-hope.github.io/v2/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 export default navbar([
-  { text: "计算机网络", icon: "network", link: "/network" },
-  {
-    text: "代码",
-    icon: "code",
-    prefix: "/",
-    children: [
-      "code/Markdown",
-      "code/AutoHotKey",
-      "code/Electron",
-      {
-        text: "页面开发",
-        icon: "vue",
-        prefix: "",
-        children: [
-          "web/VuePress",
-          "web/docsify",
-          "deploy/VPS",
-        ],
-      },
-    ],
-  }
+  { text: "计算机网络", icon: "network", link: "/network" }
 ]);
